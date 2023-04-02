44e271af79f3b5c2ea4121646360ae77fced132b
Derek Woodard <woodard.derek92@gmail.com>
2023-04-02 08:29:03 -0300

    implemented player centered camera

--- level.py
+++ level.py
@@ -13,7 +13,7 @@ class Level:
 
         # sprite group setup
         # only group that draws sprites
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = CameraGroup()
         # only group that updates sprites
         self.active_sprites = pygame.sprite.Group()
         # sprites in this group can collide with the player
@@ -31,9 +31,32 @@ class Level:
                 if col == 'X':
                     Tile((x,y),[self.visible_sprites,self.collision_sprites])
                 if col == 'P':
-                    Player((x,y),[self.visible_sprites,self.active_sprites],self.collision_sprites)
+                    self.player = Player((x,y),[self.visible_sprites,self.active_sprites],self.collision_sprites)
 
     def run(self):
         # run the entire level
         self.active_sprites.update()
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2(100,300)
+
+        # player centered camera setup
+        # self.half_w = self.display_surface.get_size()[0] //2
+        # self.half_h = self.display_surface.get_size()[1] //2
+
+        # camera box
+        
+
+    def custom_draw(self,player):
+
+        # get player offset for centered camera
+        self.offset.x = player.rect.centerx - self.half_w
+        self.offset.y = player.rect.centery - self.half_h
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
